1bc93b681398c94ff3560ac5d707ce9e749ee6ff
anand <36781763+anandMohanan@users.noreply.github.com>
2021-12-12 20:35:54 +0530

    -

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,4 @@
-// use std::{fmt::Error, io, process};
 
-// use nixinfo::{
-//     cpu, device, distro, environment, gpu, hostname, kernel, memory, packages, temp, terminal,
-//     uptime,
-// };
-
-// fn output(f: fn() -> io::Result<String>) -> String {
-//     let val = f();
-//     let val = match val {
-//         Ok(success) => success,
-//         Err(err) => {
-//             println!("error: {}", err);
-//             process::exit(1);
-//         }
-//     };
-//     val
-// }
-
-// fn main() {
-//     println!("{}", output(gpu));
-//     println!("{}", output(temp));
-// }
 mod cpu;
 mod distro;
 mod gpu;
